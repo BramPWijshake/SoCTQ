038922b69650b45b2419ef3a35a46bf7c51f6a38
arno <arno.kessel@gmail.com>
2019-10-10 14:21:29 +0200

    Update backend

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # KVTQ
 De Kolonisten van TQ
+
+## API
+
+### Format
+```json
+[
+    {"id": 1, "tile": "Resource", "type": "Sheep"},
+    {"id": 2, "tile": "Harbor", "type": "Sheep"}
+]
+```
---
+++ backend/Controllers/LayoutController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KVTQ.Controllers
+{
+    [Route("api/v1/layouts")]
+    [ApiController]
+    public class LayoutController : ControllerBase
+    {
+        // GET: api/Layout
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            string domain = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host.ToString() + HttpContext.Request.Path;
+            return new object[] {
+                new { id = 1, made = "2019-10-10T11:33:32", size = "4/5", link = domain + "/1" },
+                new { id = 2, made = "2019-10-10T11:33:32", size = "5/6", link = domain + "/2" },
+            };
+        }
+
+        // GET: api/Layout/5
+        [HttpGet("{id}", Name = "Get")]
+        public IEnumerable<object> Get(int id)
+        {
+            return new object[] {
+                new { id = 1, tile = "Resource", type = "Sheep" },
+                new { id = 2, tile = "Harbor", type = "Sheep" },
+            };
+        }
+
+        // POST: api/Layout
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Layout/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
