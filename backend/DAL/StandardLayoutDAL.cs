2f7a17860333dd032ec22da5df5a4022367f8f25
BPWijshake <b.wijshake@student.fontys.nl>
2019-10-17 14:39:31 +0200

    verder aangepast naar marc zijn wensen

--- backend/DAL/StandardLayoutDAL.cs
+++ backend/DAL/StandardLayoutDAL.cs
@@ -10,115 +10,115 @@ namespace KVTQ.DAL
     {
         string json = @"{
         'data': [{
-         'id': 1, 'tile': 'Harbor', 'type': '3for1'
+         'id': 0, 'tile': 'Harbor', 'type': '3for1'
          },
          {
-         'id': 2, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 3, 'tile': 'Harbor', 'type': '2for1Sheep'
+         'id': 0, 'tile': 'Harbor', 'type': '2for1Sheep'
          },
          {
-         'id': 4, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 5, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 6, 'tile': 'Resource', 'type': 'Forest'
+         'id': 11, 'tile': 'Resource', 'type': 'Forest'
          },
          {
-         'id': 7, 'tile': 'Resource', 'type': 'Sheep'
+         'id': 12, 'tile': 'Resource', 'type': 'Sheep'
+         },
+         {
+         'id': 9, 'tile': 'Resource', 'type': 'Grain'
+         },
+         {
+         'id': 0, 'tile': 'Harbor', 'type': '3for1'
+         },
+         {
+         'id': 0, 'tile': 'Harbor', 'type': '2for1Ore'
+         },
+         {
+         'id': 4, 'tile': 'Resource', 'type': 'Stone'
+         },
+         {
+         'id': 6, 'tile': 'Resource', 'type': 'Ore'
+         },
+         {
+         'id': 5, 'tile': 'Resource', 'type': 'Stone'
+         },
+         {
+         'id': 10, 'tile': 'Resource', 'type': 'Sheep'
+         },
+         {
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
+         },
+         {
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
+         },
+         {
+         'id': 7, 'tile': 'Resource', 'type': 'Desert'
+         },
+         {
+         'id': 3, 'tile': 'Resource', 'type': 'Forest'
+         },
+         {
+         'id': 11, 'tile': 'Resource', 'type': 'Grain'
+         },
+         {
+         'id': 4, 'tile': 'Resource', 'type': 'Forest'
          },
          {
          'id': 8, 'tile': 'Resource', 'type': 'Grain'
          },
          {
-         'id': 9, 'tile': 'Harbor', 'type': '3for1'
+         'id': 0, 'tile': 'Harbor', 'type': '3for1'
          },
          {
-         'id': 10, 'tile': 'Harbor', 'type': '2for1Ore'
+         'id': 0, 'tile': 'Harbor', 'type': '2for1Grain'
          },
          {
-         'id': 11, 'tile': 'Resource', 'type': 'Stone'
+         'id': 8, 'tile': 'Resource', 'type': 'Stone'
          },
          {
-         'id': 12, 'tile': 'Resource', 'type': 'Ore'
+         'id': 10, 'tile': 'Resource', 'type': 'Sheep'
          },
          {
-         'id': 13, 'tile': 'Resource', 'type': 'Stone'
+         'id': 9, 'tile': 'Resource', 'type': 'Sheep'
          },
          {
-         'id': 14, 'tile': 'Resource', 'type': 'Sheep'
+         'id': 3, 'tile': 'Resource', 'type': 'Ore'
          },
          {
-         'id': 15, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 16, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 17, 'tile': 'Resource', 'type': 'Desert'
+         'id': 5, 'tile': 'Resource', 'type': 'Ore'
          },
          {
-         'id': 18, 'tile': 'Resource', 'type': 'Forest'
+         'id': 2, 'tile': 'Resource', 'type': 'Grain'
          },
          {
-         'id': 19, 'tile': 'Resource', 'type': 'Grain'
+         'id': 6, 'tile': 'Resource', 'type': 'Forest'
          },
          {
-         'id': 20, 'tile': 'Resource', 'type': 'Forest'
+         'id': 0, 'tile': 'Resource', 'type': '2for1Stone'
          },
          {
-         'id': 21, 'tile': 'Resource', 'type': 'Grain'
+         'id': 0, 'tile': 'Harbor', 'type': '3for1'
          },
          {
-         'id': 22, 'tile': 'Harbor', 'type': '3for1'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },
          {
-         'id': 23, 'tile': 'Harbor', 'type': '2for1Grain'
+         'id': 0, 'tile': 'Harbor', 'type': '2for1Forest'
          },
          {
-         'id': 24, 'tile': 'Resource', 'type': 'Stone'
-         },
-         {
-         'id': 25, 'tile': 'Resource', 'type': 'Sheep'
-         },
-         {
-         'id': 26, 'tile': 'Resource', 'type': 'Sheep'
-         },
-         {
-         'id': 27, 'tile': 'Resource', 'type': 'Ore'
-         },
-         {
-         'id': 28, 'tile': 'Harbor', 'type': 'Water'
-         },
-         {
-         'id': 29, 'tile': 'Harbor', 'type': 'Water'
-         },
-         {
-         'id': 30, 'tile': 'Resource', 'type': 'Ore'
-         },
-         {
-         'id': 31, 'tile': 'Resource', 'type': 'Grain'
-         },
-         {
-         'id': 32, 'tile': 'Resource', 'type': 'Forest'
-         },
-         {
-         'id': 33, 'tile': 'Resource', 'type': '2for1Stone'
-         },
-         {
-         'id': 34, 'tile': 'Harbor', 'type': '3for1'
-         },
-         {
-         'id': 35, 'tile': 'Harbor', 'type': 'Water'
-         },
-         {
-         'id': 36, 'tile': 'Harbor', 'type': '2for1Forest'
-         },
-         {
-         'id': 37, 'tile': 'Harbor', 'type': 'Water'
+         'id': 0, 'tile': 'Harbor', 'type': 'Water'
          },]
        }";
 
