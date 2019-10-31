32b74dd66d860cf2784710c04c72214633b92a8a
joostbee <j.burggraaff@student.fontys.nl>
2019-10-31 14:13:36 +0100

    jwz

---
+++ backend/Logic/Harbor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KVTQ
+{
+    public class Harbor : Tile
+    {
+        public int alignment { get; } // in discrete stappen van graden?
+
+        //public void CalculateAlignment() ???
+    }
+}
