4a196cab788ca2fe8ee44bf0fad9b36a77ce4d25
BPWijshake <b.wijshake@student.fontys.nl>
2019-11-07 13:18:37 +0100

    Connection layer

--- backend/Controllers/LayoutController.cs
+++ backend/Controllers/LayoutController.cs
@@ -2,52 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KVTQ.Controllers
 {
-    [Route("api/v1/layouts")]
-    [ApiController]
-    public class LayoutController : ControllerBase
+    [Route("/api/StandardLayout")]
+    public class StandardLayoutController : ControllerBase
     {
-        // GET: api/Layout
-        [HttpGet]
-        public IEnumerable<object> Get()
+        [HttpGet("/All")]
+        public string GetAllLayouts()
         {
-            string domain = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host.ToString() + HttpContext.Request.Path;
-            return new object[] {
-                new { id = 1, made = "2019-10-10T11:33:32", size = "4/5", link = domain + "/1" },
-                new { id = 2, made = "2019-10-10T11:33:32", size = "5/6", link = domain + "/2" },
-            };
+            string Slayouts = " ";
+
+            return Slayouts;
         }
 
-        // GET: api/Layout/5
-        [HttpGet("{id}", Name = "Get")]
-        public IEnumerable<object> Get(int id)
+        [HttpGet("{id}")]
+        public string GetLayout(int id)
         {
-            return new object[] {
-                new { id = 1, tile = "Resource", type = "Sheep" },
-                new { id = 2, tile = "Harbor", type = "Sheep" },
-            };
-        }
+            string Slayout = " ";
 
-        // POST: api/Layout
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+            return Slayout;
         }
+    }
 
-        // PUT: api/Layout/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+    [Route("/api/GenerateLayout")]
+    public class GenerateLayoutController : ControllerBase
+    {
+        [HttpGet("{type}/{size}")]
+        public string GenerateLayout(string type, int id)
         {
-        }
+            string Glayout = " ";
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Glayout;
         }
     }
 }
